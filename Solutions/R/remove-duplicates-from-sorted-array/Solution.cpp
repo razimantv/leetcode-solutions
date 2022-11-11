b938e1f3481ba0d83f86ffc07b0224b4727d4d70
Raziman T V <razimantv@gmail.com>
2022-11-11 06:38:33 +0000

    Daily

--- Solutions/R/remove-duplicates-from-sorted-array/Solution.cpp
+++ Solutions/R/remove-duplicates-from-sorted-array/Solution.cpp
@@ -3,14 +3,9 @@
 class Solution {
  public:
   int removeDuplicates(vector<int>& nums) {
-    int N = nums.size();
-    for (int i = 1; i < N;) {
-      if (nums[i] == nums[i - 1]) {
-        for (int k = i; k + 1 < N; ++k) nums[k] = nums[k + 1];
-        --N;
-      } else
-        ++i;
-    }
-    return N;
+    int N = nums.size(), j = 1;
+    for (int i = 1; i < N; ++i)
+      if (nums[i] != nums[j - 1]) nums[j++] = nums[i];
+    return j;
   }
 };
