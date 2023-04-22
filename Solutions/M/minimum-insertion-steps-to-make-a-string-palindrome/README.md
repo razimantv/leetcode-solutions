fce964300d2354cf9714e112c339cf3bf41c1b25
Raziman T V <razimantv@gmail.com>
2023-04-22 01:28:19 +0100

    Daily

---
+++ Solutions/M/minimum-insertion-steps-to-make-a-string-palindrome/README.md
@@ -0,0 +1,8 @@
+# Minimum insertion steps to make a string palindrome
+
+[Problem link](https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/)
+
+## Tags
+
+* [Dynamic programming](/README.md#Dynamic_programming) > [Array reuse](/README.md#Dynamic_programming-Array_reuse)
+* [Palindrome](/README.md#Palindrome)
---
+++ Solutions/M/minimum-insertion-steps-to-make-a-string-palindrome/Solution.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/
+
+class Solution {
+ public:
+  int minInsertions(string s) {
+    int n = s.size();
+    vector<int> prev(n), cur(n);
+    for (int m = 2; m <= n; ++m) {
+      vector<int> next(n - m + 1);
+      for (int i = 0, j = m - 1; j < n; ++i, ++j)
+        next[i] = (s[i] == s[j]) ? prev[i + 1] : (1 + min(cur[i], cur[i + 1]));
+      prev = cur;
+      cur = next;
+    }
+    return cur[0];
+  }
+};
