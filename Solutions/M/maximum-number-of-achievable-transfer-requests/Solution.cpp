eb33bfe05bd20243328af3e05ef60a7338c31323
Raziman T V <razimantv@gmail.com>
2023-07-02 03:42:52 +0100

    Daily

---
+++ Solutions/M/maximum-number-of-achievable-transfer-requests/Solution.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/maximum-number-of-achievable-transfer-requests/
+
+class Solution {
+ public:
+  int maximumRequests(int n, vector<vector<int>>& requests) {
+    int l = requests.size(), ret{};
+    for (int mask = 1; mask < (1 << l); ++mask) {
+      int bits = __builtin_popcount(mask);
+      if (bits <= ret) continue;
+      vector<int> delta(n);
+      int bad{};
+      for (int i = 0; i < l; ++i) {
+        if (!(mask & (1 << i))) continue;
+        int after = ++delta[requests[i][0]];
+        bad += (after == 1) - (after == 0);
+        after = --delta[requests[i][1]];
+        bad += (after == -1) - (after == 0);
+      }
+      if (!bad) ret = bits;
+    }
+    return ret;
+  }
+};
