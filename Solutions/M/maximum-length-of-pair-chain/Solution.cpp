5bb6e0002e9761206dadf9ac7c755276095be9b1
Raziman T V <razimantv@gmail.com>
2023-08-26 06:35:33 +0100

    Daily: ChatGPT

---
+++ Solutions/M/maximum-length-of-pair-chain/Solution.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/maximum-length-of-pair-chain/
+
+class Solution {
+ public:
+  // ChatGPT solution
+  int findLongestChain(vector<vector<int>>& pairs) {
+    sort(pairs.begin(), pairs.end(),
+         [](const vector<int>& p1, const vector<int>& p2) {
+           return p1[1] < p2[1];
+         });
+
+    int longestChain = 1;
+    int end = pairs[0][1];
+
+    for (int i = 1; i < pairs.size(); ++i) {
+      if (pairs[i][0] > end) {
+        longestChain++;
+        end = pairs[i][1];
+      }
+    }
+
+    return longestChain;
+  }
+};
