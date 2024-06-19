e4127f8a492c9304e9b49903ec4d0d8e4f1c8026
Raziman T V <razimantv@gmail.com>
2024-06-19 05:16:20 +0100

    Daily

---
+++ Solutions/M/minimum-number-of-days-to-make-m-bouquets/Solution.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/
+
+class Solution:
+    def minDays(self, bloom: List[int], m: int, k: int) -> int:
+        n = len(bloom)
+        if m * k > n:
+            return -1
+
+        def work(day):
+            full, cur = 0, 0
+            for x in bloom:
+                if x > day:
+                    cur = -1
+                cur += 1
+                if cur == k:
+                    cur = 0
+                    full += 1
+                if full == m:
+                    return True
+            return False
+
+        start, end = 0, max(bloom)
+        while end - start > 1:
+            mid = (start + end) >> 1
+            if work(mid):
+                end = mid
+            else:
+                start = mid
+        return end
